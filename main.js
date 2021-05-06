7ac85b2080946bc1525d66e2c96afa228793e2e9
Craig Wall <craigtwall@yahoo.com>
2021-05-05 22:26:01 -0400

    Update main.js
    Older copy of main.js, I'd changed the FunctionApp and never updated the FunctionApi to match.

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 });
 
-const functionApi = 'https://craigtwall-functionapp.azurewebsites.net/api/HttpTrigger1?code=vslynYyIqCHFagj1gsKuxRdBiCyEl0a2bevFqaNI8K5Yy0hII6JxEA=='; 
+const functionApi = 'https://visitorscount.azurewebsites.net/api/HttpTrigger1?code=MToOWt/DrkPf6691/5HNogU0uN6XsdYiHjrQ5y7emqZU53Y/7hsVDA=='; 
 
 const getVisitCount = () => {
     let count = 30;
